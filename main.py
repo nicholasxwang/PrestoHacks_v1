6f1b79283ef3825ece5abca95b874f853c57f866
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-12-11 19:00:56 +0000

    up[date!

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ start_time = time.time()
 options = Options()
 browser = webdriver.Firefox(executable_path="./geckodriver",options=options)
 def main_program(image):
-  return get_image_name(image)
+  a = str(get_image_name(image))
+  s = ""
+  s+=str(a)
+  s+="\n Best Match: "
+  b = str(find_music(a))
+  s+=str(b)
+  return s
 def get_image_name(image_url):
   browser.get("https://www.google.com/searchbyimage?site=search&sa=X&image_url="+image_url)
-  time.sleep(5)
+  time.sleep(2)
   try:
-    parsed = browser.find_elements_by_class_name("fKDtNb")[0]
-  except:
-    parsed = "No Results!"
+    parsed = browser.find_elements_by_class_name("fKDtNb")[0].text
+  except Exception as e:
+    parsed = "No Results! (Exception: "+str(e)+")"
   return parsed
 def find_music(query):
-  #rishaan
-  return f"https://www.musixmatch.com/{query}/tracks"
+  browser.get(f"https://www.musixmatch.com/search/{query}/tracks")
+  time.sleep(2)
+  a = browser.find_elements_by_class_name("title")[0].text
+  return a
+  
 print(main_program("https://media.discordapp.net/attachments/905242375549509675/919289010122280980/IMG_2872.jpeg"))
 
 #Permissions Fix: chmod +x geckodriver
