2bbfd893e51cbc439b6b167c68991455f757f7e6
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-12-11 18:12:28 +0000

    update

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
+import time
 from bs4 import BeautifulSoup as bS
 def main_program(image):
   return get_image_name(image)
 def get_image_name(image_url):
   myRequest = requests.get("https://www.google.com/searchbyimage?site=search&sa=X&image_url="+image_url)
+  time.sleep(5)
+  print("https://www.google.com/searchbyimage?site=search&sa=X&image_url="+image_url)
   parsed = bS(myRequest.text, 'html.parser')
   return parsed
 print(main_program("https://cdn.discordapp.com/attachments/905242375549509675/919289010122280980/IMG_2872.jpeg"))
